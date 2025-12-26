731eeafd61bf3372ee3ab167b43abce323130dfe
ansonchow04 <ansonchow2004@outlook.com>
2025-12-26 15:22:04 +0800

    Chenpi

--- ImgProcesser/example.py
+++ ImgProcesser/example.py
@@ -1,32 +1,44 @@
-from ImgProcesser.ImgProcesser import Img
+from ImgProcesser import Img
 import cv2
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 coords = list()
 
-for i in tqdm(range(1, 114)):
-# if True:
-    # i = 50
-    img_path = f'datasetALL/{i}.bmp'
-    img = Img(path=img_path)
-    plt.figure()
+# for i in tqdm(range(1, 114)):
+#     img_path = f'datasetALL/{i}.bmp'
+#     img = Img(path=img_path)
+#     plt.figure()
 
-    plt.subplot(1, 2, 1)
-    img.show(title="Original")
+#     plt.subplot(1, 2, 1)
+#     img.show(title="Original")
 
-    plt.subplot(1, 2, 2)
-    img.gray().\
-        blur(method="median", ksize=5).\
-        otsu(inverse=True).\
-        open(ksize=3, shape="ellipse").\
-        remove_small_objects(min_area=50000)
-    coords.append(img.centroid(min_area=50000, draw=True))
-    img.show(title="Processed")
+#     plt.subplot(1, 2, 2)
+#     img.gray().\
+#         blur(method="median", ksize=5).\
+#         otsu(inverse=True).\
+#         open(ksize=3, shape="ellipse").\
+#         remove_small_objects(min_area=50000)
+#     coords.append(img.centroid(min_area=50000, draw=True))
+#     img.show(title="Processed")
     
-    plt.tight_layout()
-    plt.savefig(f'processed_image/processed_{i}{coords[-1]}.jpg', dpi=600)
-    # plt.show()
-    plt.close()
+#     plt.tight_layout()
+#     plt.savefig(f'processed_image/processed_{i}{coords[-1]}.jpg', dpi=600)
+#     plt.close()
 
-# print(coords)
+img_path = f'ImgProcesser/ChenpiData/1.bmp'
+img = Img(path=img_path)
+plt.figure()
+plt.subplot(1, 2, 1)
+img.show(title="Original")
+plt.subplot(1, 2, 2)
+img.gray().\
+    blur(method="median", ksize=5).\
+    otsu(inverse=True).\
+    open(ksize=3, shape="ellipse").\
+    remove_small_objects(min_area=50000)
+coords.append(img.centroid(min_area=50000, draw=True))
+img.show(title="Processed")
+plt.tight_layout()
+plt.savefig(f'ImgProcesser/ChenpiData/processed_1.jpg', dpi=600)
+plt.close()
